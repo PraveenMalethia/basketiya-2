4e6c8c8eada2ea7b447c3249408d519ce0d8e832
srikarkandula <79042534+srikarkandula@users.noreply.github.com>
2021-07-11 15:06:49 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <Hero/>
   <section class="text-gray-400 bg-gray-900 body-font">
     <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap -m-4">
+      <div class="flex flex-wrap justify-center -m-4">
         <div v-for="product in products" :key="product.id">
           <Product :product="product" />
         </div>
