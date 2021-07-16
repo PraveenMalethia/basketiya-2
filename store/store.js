985909292a4add693520367aa57391745823e1f4
srikarkandula <79042534+srikarkandula@users.noreply.github.com>
2021-07-16 09:37:35 +0530

    Update store.js

--- store/store.js
+++ store/store.js
@@ -3,62 +3,158 @@ export const state = () => ({
     {
       id: 1,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 2,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 3,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 4,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 5,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 6,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 7,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 8,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 9,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 10,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 11,
       name: 'Shoe',
-      img: '/images/product1.jpg'
+      img: '/images/product1.jpg',
+      imgs: [
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     },
     {
       id: 12,
       name: 'Shoe',
-      img: '/images/product2.jpg'
+      img: '/images/product2.jpg',
+      imgs: [
+        '/images/product2.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg',
+        '/images/product1.jpg'
+      ]
     }
   ]
 })
