181ad608f32774505dd4948ac0854dd6823982c0
ParveenMalethia <prince.malethia.36@gmail.com>
2021-07-16 13:38:16 +0530

    logo added

--- components/Header.vue
+++ components/Header.vue
@@ -6,14 +6,17 @@
         mx-auto
         flex flex-wrap
         p-5
-        md:justify-start
-        md:justify-between
+        md:justify-start md:justify-between
         justify-end
         flex-row
         items-center
       "
     >
-      <div @click="side_nav =! side_nav" class="absolute top-6 left-4 mr-4 text-white block md:hidden" :class="{'opacity-0 relative':side_nav}">
+      <div
+        @click="side_nav = !side_nav"
+        class="absolute top-6 left-4 mr-4 text-white block md:hidden"
+        :class="{ 'opacity-0 relative': side_nav }"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -28,8 +31,10 @@
             d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
           ></path>
         </svg>
-     </div>
-      <nav class="flex hidden md:block lg:w-2/5 flex-wrap items-center text-base">
+      </div>
+      <nav
+        class="flex hidden md:block lg:w-2/5 flex-wrap items-center text-base"
+      >
         <nuxt-link to="/" class="mr-5 hover:text-white cursor-pointer">
           Home
         </nuxt-link>
@@ -60,38 +65,89 @@
           md:mb-0
         "
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
-          ></path>
-        </svg>
-        <span class="ml-3 text-lg md:text-xl block">Basketiya</span>
+        <nuxt-link to="/" class="block relative">
+          <img
+            alt="profil"
+            src="/images/logo.jpg"
+            class="mx-auto object-cover rounded-full h-16 w-16"
+          />
+        </nuxt-link>
       </a>
       <div class="lg:w-2/5 inline-flex lg:justify-end md:ml-5 lg:ml-0">
         <button
           class="block relative"
-          @focusout="profile_dropdown=false"
+          @focusout="profile_dropdown = false"
           tabindex="0"
         >
-          <img @click="profile_dropdown=!profile_dropdown" alt="profil" src="/images/user.jpg" class="z-20 mx-auto rounded-full h-12 w-12 md:h-16 md:w-16"/>
-          <transition name="toast" style="z-index:1">
-            <div v-if="profile_dropdown" class="bg-gray-700 mt-2 absolute right-7 z-20 rounded-md px-2 py-2">
-              <div class="py-3 px-4 md:w-40 lg:w-56 xl:w-64 hover:bg-gray-600 rounded-md transition ease-in-out duration-300">
+          <img
+            @click="profile_dropdown = !profile_dropdown"
+            alt="profil"
+            src="/images/user.jpg"
+            class="z-20 mx-auto rounded-full h-12 w-12 md:h-16 md:w-16"
+          />
+          <transition name="toast" style="z-index: 1">
+            <div
+              v-if="profile_dropdown"
+              class="
+                bg-gray-700
+                mt-2
+                absolute
+                right-7
+                z-20
+                rounded-md
+                px-2
+                py-2
+              "
+            >
+              <div
+                class="
+                  py-3
+                  px-4
+                  md:w-40
+                  lg:w-56
+                  xl:w-64
+                  hover:bg-gray-600
+                  rounded-md
+                  transition
+                  ease-in-out
+                  duration-300
+                "
+              >
                 <p class="text-gray-100 text-lg">Profile</p>
               </div>
-              <div class="py-3 px-4 md:w-40 lg:w-56 xl:w-64 hover:bg-gray-600 rounded-md mt-2 transition ease-in-out duration-300">
+              <div
+                class="
+                  py-3
+                  px-4
+                  md:w-40
+                  lg:w-56
+                  xl:w-64
+                  hover:bg-gray-600
+                  rounded-md
+                  mt-2
+                  transition
+                  ease-in-out
+                  duration-300
+                "
+              >
                 <p class="text-gray-100 text-lg">Orders</p>
               </div>
-              <div class="py-3 px-4 md:w-40 lg:w-56 xl:w-64 hover:bg-red-400 bg-red-600 rounded-md mt-2 transition ease-in-out duration-300">
+              <div
+                class="
+                  py-3
+                  px-4
+                  md:w-40
+                  lg:w-56
+                  xl:w-64
+                  hover:bg-red-400
+                  bg-red-600
+                  rounded-md
+                  mt-2
+                  transition
+                  ease-in-out
+                  duration-300
+                "
+              >
                 <p class="text-gray-100 text-lg">Logout</p>
               </div>
             </div>
@@ -128,45 +184,45 @@ export default {
 
 <style scoped>
 /* enter transitions */
-  .toast-enter {
-    opacity: 0;
-    transform: scaleY(0);
-    transform-origin: top center;
-  }
-  .toast-enter-to {
-    opacity: 0;
-    transform: scale(1);
-    transform-origin: top center;
-  }
-  .toast-enter-active {
-    opacity: 1;
-    z-index: 1;
-    transition: all 0.3s ease;
-  }
-  /* leave transitions */
-  .toast-leave-from {
-    transform: scaleY(1);
-  }
-  .toast-leave-to {
-    transform: scaleY(0);
-    transform-origin: top;
-  }
-  .toast-leave-active {
-    transition: transform 0.3s ease;
-  }
+.toast-enter {
+  opacity: 0;
+  transform: scaleY(0);
+  transform-origin: top center;
+}
+.toast-enter-to {
+  opacity: 0;
+  transform: scale(1);
+  transform-origin: top center;
+}
+.toast-enter-active {
+  opacity: 1;
+  z-index: 1;
+  transition: all 0.3s ease;
+}
+/* leave transitions */
+.toast-leave-from {
+  transform: scaleY(1);
+}
+.toast-leave-to {
+  transform: scaleY(0);
+  transform-origin: top;
+}
+.toast-leave-active {
+  transition: transform 0.3s ease;
+}
 
-  .sidenav-enter {
+.sidenav-enter {
   opacity: 1;
   transform: translateX(-100px);
-  }
-  .sidenav-enter-active {
-    transition: all 0.3s ease;
-  }
-  .sidenav-leave-to {
-    opacity: 0;
-    transform: translateX(-100px);
-  }
-  .sidenav-leave-active {
-    transition: all 0.3s ease;
-  }
+}
+.sidenav-enter-active {
+  transition: all 0.3s ease;
+}
+.sidenav-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.sidenav-leave-active {
+  transition: all 0.3s ease;
+}
 </style>
