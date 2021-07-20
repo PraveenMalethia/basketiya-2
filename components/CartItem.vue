e8d720095e70f4069274619f4030c73b16be3911
ParveenMalethia <prince.malethia.36@gmail.com>
2021-07-20 07:02:26 +0530

    cart page added

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,31 +1,58 @@
 <template>
-    <div class="flex items-center md:justify-between space-x-4">
+    <div
+      class="h-full
+        flex
+        items-center
+        border-gray-800 border
+        p-5
+        rounded-xl
+        hover:bg-gray-700
+        transition
+        duration-300
+        easy-in-out
+        transform hover:-translate-y-2 hover:scale-100">
         <div class="p-1 w-1/2 md:w-1/6 flex-none overflow-hidden">
           <img src="/images/product3.jpg" class="w-full h-28 md:h-32 flex-none rounded-xl md:rounded-3xl object-cover object-center">
         </div>
         <div class="flex flex-col md:flex-row md:justify-between space-y-2 md:w-5/6 h-full md:space-x-4">
-          <h3 class="text-2xl w-full md:w-2/6 font-semibold text-white">Nike Shoe</h3>
-          <p class="text-xl text-white font-semibold">10.50</p>
+          <h3 class="text-2xl w-full md:w-2/6 font-semibold text-white ml-3">Nike Shoe</h3>
+          <p class="text-xl text-white font-semibold">₹ 10.50</p>
           <div class="flex items-center justify-between">
             <div class="flex items-center space-x-1 text-gray-200 rounded-md py-1">
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </div>
-              <p class="text-xl text-white px-3">10</p>
-              <div>
+              <button>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-              </div>
+              </button>
+              <p class="text-xl text-white px-3">10</p>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </button>
             </div>
           </div>
-          <div class="text-gray-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </div>
+          <button
+            class="text-red-500
+              bg-transparent
+              border border-solid border-red-500
+              hover:bg-red-500
+              hover:text-white
+              active:bg-red-600
+              font-bold
+              uppercase
+              px-4
+              py-2
+              rounded-full
+              outline-none
+              focus:outline-none
+              mr-1
+              mb-1
+              ease-linear
+              transition-all
+              duration-150">
+            Remove
+          </button>
         </div>
     </div>
 </template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="text-gray-400 bg-gray-900 body-font">
-    <div class="px-4 pr-6 py-24 mx-auto">
-      <h1 class="text-white text-3xl font-bold mb-10">Shopping Cart</h1>
-
+  <section class="text-gray-400 bg-gray-900 body-font">
+    <div
+      class="container
+        mx-auto
+        flex
+        px-5
+        py-24
+        md:flex-row
+        flex-col
+        items-center
+        "
+      >
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Shopping Cart</h1>
       <div class="lg:grid lg:gap-3 lg:grid-cols-6 lg:flex-row space-x-4">
         <div class="col-span-4">
-          <div class="flex flex-col space-y-2 divide-y-2 divide-gray-500">
+          <div class="flex flex-col space-y-2">
             <div class="py-2">
               <CartItem />
             </div>
@@ -22,16 +31,16 @@
             <h1 class="mx-auto text-2xl justify-self-center text-white font-semibold">Order Summary</h1>
             <div class="flex items-center justify-between">
               <p class="mt-10 text-lg text-white">Order Items:</p>
-              <p class="mt-10 text-lg font-bold text-white">13</p>
+              <p class="mt-10 text-lg font-bold text-white">₹ 13</p>
             </div>
             <div class="flex items-center justify-between">
               <p class="mt-4 text-lg text-white">Total Price:</p>
-              <p class="mt-4 text-lg font-bold text-white">510</p>
+              <p class="mt-4 text-lg font-bold text-white">₹ 510</p>
             </div>
             <p class="text-gray-400 text-lg mt-7">Coupon Code:</p>
             <input
               type="text"
-              class="mt-2 rounded-md w-full px-3 py-2 text-white text-base md:w-4/6 lg:w-5/6 outline-none bg-gray-800
+              class="mt-2 rounded-md w-full px-3 py-2 text-white text-base md:w-full lg:w-full outline-none bg-gray-800
               border border-gray-700 focus:border-purple-700 focus:ring-2 focus:ring-purple-700 transition duration-150 ease-in-out shadow-sm"
             >
             <button class="mt-8 px-4 py-2 bg-purple-700 rounded-md shadow-md text-white text-lg">Proceed to Checkout</button>
@@ -39,7 +48,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script>
 export default {}
@@ -47,4 +56,3 @@ export default {}
 
 <style>
 </style>
-
