5dc85a78577eb207d7a9f4412fb381bc2f25ad08
ParveenMalethia <prince.malethia.36@gmail.com>
2021-07-23 08:09:46 +0530

    button class fix

--- pages/track-order.vue
+++ pages/track-order.vue
@@ -321,26 +321,7 @@
         </div>
         <button
           class="
-            mt-4
-            text-green-400
-            bg-transparent
-            border-2 border-solid border-green-500
-            hover:bg-green-500
-            hover:text-white
-            active:bg-emerald-600
-            font-bold
-            uppercase
-            px-8
-            py-3
-            rounded-2xl
-            outline-none
-            focus:outline-none
-            mr-1
-            mb-1
-            ease-linear
-            transition-all
-            duration-150
-            text-center
+            btn-success
           "
         >
           Track order with id {{orderId}}
