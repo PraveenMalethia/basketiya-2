4c6e1ce676c8a6cc647b4abccbf04711b9b65adf
ParveenMalethia <prince.malethia.36@gmail.com>
2021-07-24 15:37:18 +0530

    load more button  added

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div>
     <Hero />
     <Featuring />
-    <section class="text-gray-400 bg-gray-900 body-font">
+    <section class="text-gray-300 bg-gray-900 body-font">
+      <h1 class="text-center text-3xl">Featuring Products</h1>
       <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-wrap justify-center -m-4">
           <div v-for="product in products" :key="product.id">
--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <section class="text-gray-400 bg-gray-900 body-font">
+  <section class="text-gray-300 bg-gray-900 body-font">
+    <h1 class="text-center text-3xl">Welcome to Basketiya Store</h1>
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-wrap justify-center -m-4">
         <div v-for="product in products" :key="product.id">
@@ -7,17 +8,31 @@
         </div>
       </div>
     </div>
+    <div class="flex items-center justify-center">
+      <button @click="loadMore()" class="btn-secondary px-5 py-3">{{ loading ? "Loading . . ." : "Load More" }}</button>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   data: () => {
-    return {}
+    return {
+      loading: false
+    }
   },
   computed: {
     products () {
       return this.$store.state.store.products
+    }
+  },
+  methods: {
+    loadMore () {
+      // this.$store.dispatch('loadMoreProducts')
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 500)
     }
   }
 }
