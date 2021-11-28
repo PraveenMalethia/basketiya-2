29b3b1a32fcecfaa3eb81d251116c730f51108ed
ParveenMalethia <prince.malethia.36@gmail.com>
2021-11-28 22:08:46 +0530

    Wishlist Done

--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -10,174 +10,33 @@
           <b>Enter order id and get details.</b>
         </p>
       </div>
-      <div class="flex flex-wrap -m-4">
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/420x260"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              The Catalyzer
-            </h2>
-            <p class="mt-1">
-              $16.00
-            </p>
+      <section class="text-gray-400 bg-gray-900 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div v-for="product in products" :key="product.id" class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Product :product="product" />
+            </div>
           </div>
         </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/421x261"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              Shooting Stars
-            </h2>
-            <p class="mt-1">
-              $21.15
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/422x262"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              Neptune
-            </h2>
-            <p class="mt-1">
-              $12.00
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/423x263"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              The 400 Blows
-            </h2>
-            <p class="mt-1">
-              $18.40
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/424x264"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              The Catalyzer
-            </h2>
-            <p class="mt-1">
-              $16.00
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/425x265"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              Shooting Stars
-            </h2>
-            <p class="mt-1">
-              $21.15
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/427x267"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              Neptune
-            </h2>
-            <p class="mt-1">
-              $12.00
-            </p>
-          </div>
-        </div>
-        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-          <a class="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              class="object-cover object-center w-full h-full block"
-              src="https://dummyimage.com/428x268"
-            >
-          </a>
-          <div class="mt-4">
-            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
-              CATEGORY
-            </h3>
-            <h2 class="text-white title-font text-lg font-medium">
-              The 400 Blows
-            </h2>
-            <p class="mt-1">
-              $18.40
-            </p>
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
   </section>
 </template>
 
 <script>
 export default {
+  data: () => {
+    return {
+      loading: false,
+      products: []
+    }
+  },
+  async fetch () {
+    await this.$axios.get('https://fakestoreapi.com/products?limit=20')
+      .then((response) => {
+        this.products = response.data
+      })
+  },
   head: {
     title: 'Basketiya | Wishlist',
     meta: [
